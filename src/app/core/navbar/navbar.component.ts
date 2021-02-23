541190927af957ee726ccdff60a61b27d75c6f41
Chandra Panta C <chandra.panta345@hotmail.com>
2021-02-22 20:33:32 -0500

    Redirects logged in users to their profilepage when profile nav-item is clicked.

--- src/app/core/navbar/navbar.component.ts
+++ src/app/core/navbar/navbar.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import User from 'src/app/shared/models/user.model';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.sass']
+  styleUrls: ['./navbar.component.sass'],
 })
 export class NavbarComponent implements OnInit {
+  user: User | null = null;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.user = {
+      username: 'chandra-panta',
+      realName: 'Chandra Panta Chhetri',
+      city: 'Toronto',
+      country: 'Canada',
+      email: 'chandra@gmail.com',
+      profileUrl: 'https://dummyimage.com/250',
+      role: 'user',
+      bio:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+    };
   }
-
 }
