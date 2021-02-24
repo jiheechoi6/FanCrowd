ffdf65b6e2e76afb85f89b9d04121c5641ae10ff
rajpatel5 <devraj5698@yahoo.ca>
2021-02-24 00:54:59 -0500

    Created event service which event component uses

--- src/app/events/events.component.ts
+++ src/app/events/events.component.ts
@@ -1,16 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
-interface Event {
-  id: number;
-  fandomType: string;
-  name: string;
-  description: string;
-  postedBy: string;
-  location: string;
-  startDate: string;
-  endDate: string;
-  totalAttendance: number; // Not including the person that created the event
-}
+import { ActivatedRoute, Router } from '@angular/router';
+import { EventService } from '../core/services/event.service';
+import Event from '../shared/models/event.model';
 
 @Component({
   selector: 'app-events',
@@ -21,59 +12,20 @@ export class EventsComponent implements OnInit {
 
   events: Array<Event> = [];
   today = new Date();
+  isAdmin = false;
 
-  constructor() { }
+  constructor(
+    private eventService: EventService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
-    let Event1 = {
-      id: 1,
-      fandomType: "books",
-      name: "Comic Con",
-      description: `A comic book convention or comic con is an event with a 
-      primary focus on comic books and comic book culture, in which comic book 
-      fans gather to meet creators, experts, and each other. Commonly, comic conventions 
-      are multi-day events hosted at convention centers, hotels, or college campuses.`,
-      postedBy: "user1",
-      location: "Toronto, Ontario, Canada",
-      startDate: this.setDateFromToday(0),
-      endDate: this.setDateFromToday(2),
-      totalAttendance: 2,
+    this.events = this.eventService.getEvents();
+    const username: string = this.activatedRoute.snapshot.params['username'];
+    if (username && username.includes("admin")){
+      this.isAdmin = true;
     }
-
-    let Event2 = {
-      id: 2,
-      fandomType: "technology",
-      name: "World Expo",
-      description: `Our once-in-a-lifetime celebration – the largest event ever staged in the 
-      Arab world – is set to welcome 190 participating countries, and millions of visitors from 
-      across the globe. Here they will experience warm Emirati hospitality at its finest, as 
-      well as the UAE’s values of inclusion, tolerance and cooperation. Youth are at the heart 
-      of our World Expo. That’s why Expo 2020 aspires to create a meaningful legacy that will 
-      benefit generations to come, both locally and globally, spanning everything from innovations 
-      and architecture to friendships and business opportunities.`,
-      postedBy: "user2",
-      location: "Dubai, UAE",
-      startDate: this.setDateFromToday(-1),
-      endDate: this.setDateFromToday(1),
-      totalAttendance: 0,
-    }
-
-    let Event3 = {
-      id: 3,
-      fandomType: "movies",
-      name: "Marvel Studios",
-      description: `Marvel Studios, LLC is an American film and television studio that is a 
-      subsidiary of Walt Disney Studios, a division of The Walt Disney Company. Marvel Studios 
-      is known for the production of the Marvel Cinematic Universe films, based on characters 
-      that appear in Marvel Comics publications.`,
-      postedBy: "admin",
-      location: "San Fransico, California, USA",
-      startDate: this.setDateFromToday(-2),
-      endDate: this.setDateFromToday(3),
-      totalAttendance: 1,
-    }
-
-    this.events = [Event1, Event2, Event3];
   }
 
   setDateFromToday(offset: number): string{
@@ -86,7 +38,7 @@ export class EventsComponent implements OnInit {
 
   // This should be done in a modal window, but it's here for now
   create(): void{
-    let Event4 = {
+    let event = {
       id: 4,
       fandomType: "shows",
       name: "Game of Thrones Convention",
@@ -100,14 +52,12 @@ export class EventsComponent implements OnInit {
       totalAttendance: 2,
     }
 
-    this.events.push(Event4);
+    this.eventService.createEvent(event);
+    this.events = this.eventService.getEvents();
   }
 
   delete(id: number): void{
     let index = this.events.findIndex(event => event.id === id);
-    
-    if (index >= 0){
-      this.events.splice(index, 1);
-    }
+    this.eventService.deleteEvent(index);
   }
 }
