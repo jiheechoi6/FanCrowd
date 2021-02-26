811418ee04f278a79fedee21f612e002e44a5fdc
Chandra Panta C <chandra.panta345@hotmail.com>
2021-02-26 17:16:07 -0500

    Fixes styling issue for login and signup. Fixes broken discussion-board link.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,34 +7,32 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.sass']
+  styleUrls: ['./login.component.sass'],
 })
 export class LoginComponent implements OnInit {
-  username = ""
-  password = ""
-  loginfailed = false
-  errorMessage = ""
+  username = '';
+  password = '';
+  loginfailed = false;
+  errorMessage = '';
 
-  constructor(
-    private authService: AuthService,
-    private router: Router,
-  ) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  onLogIn(){
-    if(this.authService.getPasswordByUsername(this.username) == this.password){
-      this.authService.currentLoggedInUser = this.username
-      this.loginfailed = false
-      this.router.navigate(['/events'])
-    }else{
-      if(this.username == "" || this.password == ""){
-        this.errorMessage = "Please enter your username and password"
-      }else{
-        this.errorMessage = "Your username or password is wrong"
+  onLogIn() {
+    if (
+      this.authService.getPasswordByUsername(this.username) == this.password
+    ) {
+      this.authService.currentLoggedInUser = this.username;
+      this.loginfailed = false;
+      this.router.navigate(['/events']);
+    } else {
+      if (this.username == '' || this.password == '') {
+        this.errorMessage = 'Please enter your username and password';
+      } else {
+        this.errorMessage = 'Your username or password is wrong';
       }
-      this.loginfailed = true
+      this.loginfailed = true;
     }
   }
 }
