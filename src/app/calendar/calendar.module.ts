0bd9a5292c61a32441dac7b27f230475c86184c6
Jihee Choi <christine.choi@mail.utoronto.ca>
2021-02-27 03:50:25 +0900

    added calendar

--- src/app/calendar/calendar.module.ts
+++ src/app/calendar/calendar.module.ts
@@ -1,10 +1,15 @@
-import { NgModule } from '@angular/core';
+import { NgModule, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CalendarComponent } from './calendar.component';
 import { CalendarRoutingModule } from './calendar-routing.module';
+import { CoreModule } from '../core/core.module';
+import { SharedModule } from '../shared/shared.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [CalendarComponent],
-  imports: [CommonModule, CalendarRoutingModule],
+  imports: [CommonModule, CalendarRoutingModule, CoreModule, SharedModule],
 })
-export class CalendarModule {}
+export class CalendarModule{
+
+}
