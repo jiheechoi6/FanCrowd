ed35944b77c59a98c31f5111f915c3b9e5df2a35
Jihee Choi <christine.choi@mail.utoronto.ca>
2021-03-02 22:58:51 +0900

    added highlight for today's date

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -12,16 +12,17 @@ export class CalendarComponent implements OnInit{
 
   months = ["January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
   today:any = new Date();
-  displayMonth:number = this.today.getMonth()
+  showdate:any = new Date();
+  displayMonth:number = this.showdate.getMonth()
   displayMonthString:string = this.months[this.displayMonth]
-  displayYear:number = this.today.getFullYear()
+  displayYear:number = this.showdate.getFullYear()
   
   displayFirstDay = new Date(this.displayYear, this.displayMonth, 1).getDay()
   displayLastDate = new Date(this.displayYear, this.displayMonth+1, 0).getDate()
   displayLastDay = new Date(this.displayYear, this.displayMonth, this.displayLastDate)
   displayPrevMonthLastDate = new Date(this.displayYear, this.displayMonth, 0).getDate()
 
-  fullDate = this.today.toDateString()
+  fullDate = this.showdate.toDateString()
 
   constructor(
     private authService: AuthService,
@@ -32,24 +33,24 @@ export class CalendarComponent implements OnInit{
   }
 
   updateDateValues(){
-    this.displayMonth= this.today.getMonth()
+    this.displayMonth= this.showdate.getMonth()
     this.displayMonthString = this.months[this.displayMonth]
-    this.displayYear = this.today.getFullYear()
+    this.displayYear = this.showdate.getFullYear()
     
     this.displayFirstDay = new Date(this.displayYear, this.displayMonth, 1).getDay()
     this.displayLastDate = new Date(this.displayYear, this.displayMonth+1, 0).getDate()
     this.displayLastDay = new Date(this.displayYear, this.displayMonth, this.displayLastDate)
     this.displayPrevMonthLastDate = new Date(this.displayYear, this.displayMonth, 0).getDate()
-    this.fullDate = this.today.toDateString()
+    this.fullDate = this.showdate.toDateString()
   }
 
   prevMonth(){
-    this.today.setMonth(this.today.getMonth()-1)
+    this.showdate.setMonth(this.showdate.getMonth()-1)
     this.updateDateValues()
   }
 
   nextMonth(){
-    this.today.setMonth(this.today.getMonth()+1)
+    this.showdate.setMonth(this.showdate.getMonth()+1)
     this.updateDateValues()
   }
   
@@ -83,6 +84,16 @@ export class CalendarComponent implements OnInit{
       backdropClass: 'material-dialog-backdrop',
       width: '400px',
     });
+  }
+
+  isToday(date:number){
+    if(this.displayYear ==this.today.getFullYear() &&
+      this.displayMonth == this.today.getMonth() &&
+      date == this.today.getDate()){
+        return true;
+      }else{
+        return false;
+      }
   }
 
   counter(i: number) {
