43b3a53cdcf874b60ba58d399c1905f8ef904a15
Jihee Choi <christine.choi@mail.utoronto.ca>
2021-03-07 21:52:00 +0900

    added filter. not connected to data yet

--- src/app/core/navbar/navbar.component.ts
+++ src/app/core/navbar/navbar.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { Subscription, Observable } from 'rxjs';
 import UserDTO from 'src/app/shared/models/user-dto';
 import { AuthService } from '../services/auth.service';
+import { FormControl } from '@angular/forms';
+import { startWith, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-navbar',
@@ -11,7 +13,10 @@ import { AuthService } from '../services/auth.service';
 export class NavbarComponent implements OnInit, OnDestroy {
   userSubscription!: Subscription;
   user: UserDTO | null = null;
-  options = [1, 2, 3];
+
+  myControl = new FormControl();
+  options:string[] = ['one', 'two', 'three'];
+  filteredOptions: Observable<string[]> | undefined;
 
   constructor(private _authService: AuthService) {}
 
@@ -19,6 +24,12 @@ export class NavbarComponent implements OnInit, OnDestroy {
     this.userSubscription = this._authService.currentUser.subscribe(
       (user) => (this.user = user)
     );
+
+    this.filteredOptions = this.myControl.valueChanges
+      .pipe(
+        startWith(''),
+        map(value => this._filter(value))
+      );
   }
 
   ngOnDestroy(): void {
@@ -27,5 +38,13 @@ export class NavbarComponent implements OnInit, OnDestroy {
 
   onLogOut() {
     this._authService.logOut();
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    console.log(this.options.filter(option => option.toLowerCase().includes(filterValue)))
+    this.filteredOptions
+    return this.options.filter(option => option.toLowerCase().includes(filterValue));
   }
 }
