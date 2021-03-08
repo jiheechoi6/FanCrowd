02cbaedecfe790f096a20538279028a4c9427e29
Chandra Panta C <chandra.panta345@hotmail.com>
2021-03-07 21:05:09 -0500

    Removes calendar routing module and moves to calendar module. Fixes username styling in navbar.

--- src/app/calendar/calendar.module.ts
+++ src/app/calendar/calendar.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CalendarComponent } from './calendar.component';
-import { CalendarRoutingModule } from './calendar-routing.module';
 import { CoreModule } from '../core/core.module';
 import { SharedModule } from '../shared/shared.module';
 import { DateEventDialogComponent } from './date-event-dialog/date-event-dialog.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [{ path: '', component: CalendarComponent }];
 
 @NgModule({
   declarations: [CalendarComponent, DateEventDialogComponent],
-  imports: [CommonModule, CalendarRoutingModule, CoreModule, SharedModule],
+  imports: [
+    CommonModule,
+    CoreModule,
+    SharedModule,
+    RouterModule.forChild(routes),
+  ],
+  exports: [RouterModule],
 })
 export class CalendarModule {}
