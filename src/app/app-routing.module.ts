72b750ce23fe0fd8f43095ab66231e7b1ac23ef7
Chandra Panta C <chandra.panta345@hotmail.com>
2021-03-07 22:35:31 -0500

    Moves guards to separate folder.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AuthGuard } from './core/services/auth.guard';
-import { LoggedInGuard } from './core/services/logged-in.guard';
+import { AuthGuard } from './core/guards/auth.guard';
+import { LoggedInGuard } from './core/guards/logged-in.guard';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
--- src/app/core/guards/logged-in.guard.ts
+++ src/app/core/guards/logged-in.guard.ts
@@ -8,7 +8,7 @@ import {
 } from '@angular/router';
 import { Observable } from 'rxjs';
 import { map, take } from 'rxjs/operators';
-import { AuthService } from './auth.service';
+import { AuthService } from '../services/auth.service';
 
 @Injectable({ providedIn: 'root' })
 export class LoggedInGuard implements CanActivate {
