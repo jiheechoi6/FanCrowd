a6efbf128d4eab31046e675e3de75fa4a04ccd67
Chandra Panta C <chandra.panta345@hotmail.com>
2021-03-09 12:29:56 -0500

    Fixes bio form field in edit user modal. Fixes error messages for edit-review.

--- src/app/events/edit-review-dialog/edit-review-dialog.component.ts
+++ src/app/events/edit-review-dialog/edit-review-dialog.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { EventService } from 'src/app/core/services/event.service';
-import Review from 'src/app/shared/models/review';
 
 @Component({
   selector: 'app-edit-review-dialog',
@@ -13,15 +12,14 @@ export class EditReviewDialogComponent implements OnInit {
 
   constructor(
     public dialogRef: MatDialogRef<EditReviewDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: {eventId: number, review: any},
+    @Inject(MAT_DIALOG_DATA) public data: { eventId: number; review: any },
     private eventService: EventService
-  ) {
-  }
+  ) {}
 
   ngOnInit(): void {}
 
   onUpdateReview() {
-    if (this.data.review){
+    if (this.data.review) {
       this.eventService.updateReviewById(this.data.eventId, this.data.review);
       this.dialogRef.close(this.data.review);
     }
