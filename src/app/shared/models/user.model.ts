7f3af2de5e92637698d95cb61bc4a2e859899151
rajpatel5 <devraj5698@yahoo.ca>
2021-03-15 18:09:10 -0400

    Modified files to remove merge tags

--- src/app/shared/models/user.model.ts
+++ src/app/shared/models/user.model.ts
@@ -1,11 +1,7 @@
-<<<<<<< HEAD:src/app/shared/models/user.model.ts
-interface User {
-=======
 import FandomDTO from './fandom-dto';
 import EventDTO from './event-dto';
 
 interface UserDTO {
->>>>>>> af65f413551de5e189f3a5eb30e53185d6499dc7:src/app/shared/models/user-dto.ts
   username: string;
   fullName: string;
   email: string;
@@ -14,11 +10,8 @@ interface UserDTO {
   bio: string;
   profileUrl: string;
   role: string;
-<<<<<<< HEAD:src/app/shared/models/user.model.ts
-=======
   attendingEvents: EventDTO[];
   fandoms: FandomDTO[];
->>>>>>> af65f413551de5e189f3a5eb30e53185d6499dc7:src/app/shared/models/user-dto.ts
 }
 
 export default UserDTO;
