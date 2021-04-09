d26b43654bcc7c77bc4383197e2e62475f7a9b59
Chandra Panta C <chandra.panta345@hotmail.com>
2021-04-09 14:14:39 -0400

    Fixes build process for angular.

--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -11,9 +11,9 @@ export default async ({ app }: { app: express.Application }) => {
   app.use(express.json());
 
   if (config.env === "production") {
-    app.use(express.static(path.join(__dirname, "client/build")));
+    app.use(express.static(path.join(__dirname, "client/dist")));
     app.get("/", (req, res) =>
-      res.sendFile(path.join(__dirname, "client/build/index.html"))
+      res.sendFile(path.join(__dirname, "client/dist/index.html"))
     );
   }
 
