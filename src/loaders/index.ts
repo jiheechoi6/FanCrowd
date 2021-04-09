87230c8a6de9bf8ed60c22b50b3139040b703a4c
Chandra Panta C <chandra.panta345@hotmail.com>
2021-04-09 16:10:10 -0400

    Removes loading seed file.

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -1,10 +1,8 @@
 import express from "express";
 import expressLoader from "./express";
 import mongooseLoader from "./mongoose";
-import seedDb from "./seed-db";
 
 export default async ({ expressApp }: { expressApp: express.Application }) => {
   await mongooseLoader();
-  await seedDb();
   await expressLoader({ app: expressApp });
 };
