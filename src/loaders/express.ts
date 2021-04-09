ed9edc27bcbea762837901eedc61ea5e5838ae32
Chandra Panta C <chandra.panta345@hotmail.com>
2021-04-09 15:40:19 -0400

    Fixes opacity styling for fandom pages.

--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -49,7 +49,6 @@ export default async ({ app }: { app: express.Application }) => {
     } else if (err.name === "NotFoundError") {
       return res.status(404).send({ message: err.message }).end();
     }
-    console.log(err);
     res.status(500).send({ message: "Internal Server Error" }).end();
   });
 };
