512f6a83952eb7204432946638f6e79f98e7312a
Chandra Panta C <chandra.panta345@hotmail.com>
2021-04-09 16:05:17 -0400

    Adds seed loader.

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import expressLoader from "./express";
 import mongooseLoader from "./mongoose";
+import seedDb from "./seed-db";
 
 export default async ({ expressApp }: { expressApp: express.Application }) => {
   await mongooseLoader();
+  await seedDb();
   await expressLoader({ app: expressApp });
 };
